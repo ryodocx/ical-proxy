d90371f5de997c353a49443ae0589b6631b8c82d
ryodocx <email@ryodocx.net>
2022-07-16 21:08:51 +0900

    update

--- pkg/util/error.go
+++ pkg/util/error.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-func WrapError(err error, msg string) error {
+func WrapError(err error, msg ...string) error {
 	pc, f, l, _ := runtime.Caller(1)
 	fn := runtime.FuncForPC(pc)
 	return fmt.Errorf("%s: %+v\n%s@\n%s:%d", msg, err, fn.Name(), f, l)
